8645f028189068cc19bcc1e5e83609adf289e2be
Alexis Perrier <alexis.perrier@gmail.com>
2023-07-05 14:38:32 +0200

    verse_id

--- script/parse_AVR.py
+++ script/parse_AVR.py
@@ -43,21 +43,21 @@ class Play():
         if line.category in ['acte','character_list']:
             ps.items.append(line.to_dict(
                 **{
-                    'verse': 0
+                    'verse_id': 0
                 }
             ))
         elif line.category == 'scene':
             ps.items.append(line.to_dict(
                 **{
                     'text': line.text.split('-')[0].strip(),
-                    'verse': 0
+                    'verse_id': 0
                 }
             ))
             ps.items.append(line.to_dict(
                 **{
                     'text': line.text.split('-')[1].strip(),
                     'category': 'character_list',
-                    'verse': 0
+                    'verse_id': 0
                 }
             ))
 
@@ -68,13 +68,13 @@ class Play():
         par = self.items.copy()
 
         par['paragraph'] = par.groupby(
-            by = ['category', 'acte', 'scene', 'verse', 'subverse']
+            by = ['category', 'acte', 'scene', 'verse_id', 'subverse_id']
         )['text'].transform(lambda x: ' '.join(x))
 
 
-        par.drop_duplicates(subset = ['category', 'acte', 'scene', 'verse', 'subverse', 'paragraph'],inplace = True)
+        par.drop_duplicates(subset = ['category', 'acte', 'scene', 'verse_id', 'subverse_id', 'paragraph'],inplace = True)
         par['text'] = par.paragraph
-        self.items = par[['category', 'acte', 'scene', 'verse', 'subverse', 'pos', 'text']].copy()
+        self.items = par[['category', 'acte', 'scene', 'verse_id', 'subverse_id', 'pos', 'text']].copy()
         self.items.reset_index(inplace = True, drop = True)
 
     def save(self):
@@ -146,8 +146,8 @@ class Line(Play):
             "category": self.category,
             "acte": Play.acte_count,
             "scene": Play.scene_count,
-            "verse": Play.verse_count,
-            "subverse": Play.subverse_count,
+            "verse_id": Play.verse_count,
+            "subverse_id": Play.subverse_count,
             "pos": self.pos,
             "text": self.text,
         }
@@ -170,8 +170,8 @@ if __name__ == "__main__":
     assert ps.items[ps.items.category == 'acte'].shape[0] == 5
     assert ps.items[ps.items.category == 'scene'].shape[0] == 44
     assert ps.items.scene.max() == 15
-    assert ps.items.verse.max() == 967
-    assert ps.items.subverse.max() == 3
+    assert ps.items.verse_id.max() == 967
+    assert ps.items.subverse_id.max() == 3
     assert ps.items[ps.items.category == 'character_list'].shape[0] == 44
     assert ps.items[ps.items.category == 'character'].text.value_counts().shape[0] == 13
     assert np.alltrue(
@@ -185,139 +185,10 @@ if __name__ == "__main__":
     # paragraphize
     ps.concat_verse()
 
-    assert ps.items.verse.value_counts().shape[0] == 967 + 1
-    assert ps.items.subverse.max() == 3
-    assert np.alltrue( ps.items.subverse.value_counts().values == [1917, 265, 43, 7])
-    assert np.alltrue( ps.items.verse.value_counts().head(10).values == [93,  8,  7,  7,  6,  6,  6,  6,  6,  6])
+    assert ps.items.verse_id.value_counts().shape[0] == 967 + 1
+    assert ps.items.subverse_id.max() == 3
+    assert np.alltrue( ps.items.subverse_id.value_counts().values == [1917, 265, 43, 7])
+    assert np.alltrue( ps.items.verse_id.value_counts().head(10).values == [93,  8,  7,  7,  6,  6,  6,  6,  6,  6])
 
 
     ps.save()
-
-    # par = ps.items.copy()
-    #
-    # par['paragraph'] = par.groupby(
-    #     by = ['category', 'acte', 'scene', 'verse', 'subverse']
-    # )['text'].transform(lambda x: ','.join(x))
-    #
-    #
-    # par.drop_duplicates(subset = ['category', 'acte', 'scene', 'verse', 'subverse', 'paragraph'],inplace = True)
-    # par['text'] = par.paragraph
-    # par = par[['category', 'acte', 'scene', 'verse', 'subverse', 'pos', 'text']]
-
-
-
-    #     if "ACTE " in line:
-    #         # reset scene count
-    #         acte_count += 1
-    #         scene_count = 0
-    #         items.append(
-    #             {
-    #                 "category": "acte",
-    #                 "acte": acte_count,
-    #                 "scene": 1,
-    #                 "pos": n ,
-    #                 "text": line,
-    #             }
-    #         )
-    #     elif "Scène" in line:
-    #         scene_str = line.split('-')[0].strip()
-    #         char_list = line.split('-')[1].strip()
-    #
-    #         scene_count += 1
-    #         items.append(
-    #             {
-    #                 "category": "scene",
-    #                 "acte": acte_count,
-    #                 "scene": scene_count,
-    #                 "pos": n ,
-    #                 "text": scene_str,
-    #             }
-    #         )
-    #         items.append(
-    #             {
-    #                 "category": "char_list",
-    #                 "acte": acte_count,
-    #                 "scene": scene_count,
-    #                 "pos": n ,
-    #                 "text": char_list,
-    #             }
-    #
-    #         )
-    #
-    #     elif re.search(char_pattern, line):
-    #         char_name = line[re.search(char_pattern, line).start():re.search(char_pattern, line).end()]
-    #         items.append(
-    #             {
-    #                 "category": "character",
-    #                 "acte": acte_count,
-    #                 "scene": scene_count,
-    #                 "pos": n ,
-    #                 "text": char_name,
-    #             }
-    #         )
-    #     elif re.search(action_pattern, line):
-    #         action_str = line[re.search(action_pattern, line).end(): -1]
-    #         items.append(
-    #             {
-    #                 "category": "action",
-    #                 "acte": acte_count,
-    #                 "scene": scene_count,
-    #                 "pos": n ,
-    #                 "text": action_str,
-    #             }
-    #         )
-    #     else:
-    #         paragraph = []
-    #         start_n = n
-    #         while (line.strip() != "") & (n < len(lines)):
-    #             paragraph.append(line)
-    #             try:
-    #                 n +=1
-    #                 line = lines[n].strip()
-    #             except:
-    #                 pass
-    #         items.append(
-    #             {
-    #                 "category": "replique",
-    #                 "acte": acte_count,
-    #                 "scene": scene_count,
-    #                 "pos": start_n ,
-    #                 "text": " ".join(paragraph),
-    #             }
-    #         )
-    #
-    # items = pd.DataFrame(items)
-    # items = items[items.text != ""].copy()
-    #
-    #
-    #
-    # # post parsing
-    # # remove []
-    # items["text"] = items.text.apply(lambda t: re.sub(r"\(\d+\)", "", t))
-    #
-    # # verse_id
-    # verse_id = 0
-    # for i, d in items.iterrows():
-    #     if d["category"] == "character":
-    #         verse_id += 1
-    #     items.loc[i, "verse_id"] = verse_id
-    # items["verse_id"] = items.verse_id.astype(int)
-    #
-    # items.loc[items.category == 'acte', 'verse_id' ] = 0
-    # items.loc[items.category == 'scene', 'verse_id' ] = 0
-    # items.loc[items.category == 'character list', 'verse_id' ] = 0
-    #
-    # # anomaly: "== ACTE ..." as replique => delete
-    # # items = items.drop(index = items[(items.category == 'replique') & (items.text.str.contains('== ACTE'))].index).copy()
-    # items.reset_index(drop = True, inplace = True)
-    #
-    # items = items[['pos','acte', 'scene', 'category', 'verse_id', 'text']]
-    #
-    # # save
-    # with open(output_filename, "w", encoding="utf-8") as f:
-    #     items.to_json(f, force_ascii=False, orient="records", indent=4)
-
-
-
-def test(**kwargs):
-    print(kwargs)
